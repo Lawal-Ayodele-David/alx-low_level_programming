701c7cb6063c4db1afcc6665c2306e377b397e7f
lawal-ayodele-david <lawalayodeledavid1@gmail.com>
2023-06-19 17:45:42 -0700

    Programming using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *return: always 0 (success)
+  *main - entry points "Programming is like building a multilingual puzzle"
+  *Return: always 0 (success)
   */
 int main(void)
 {
