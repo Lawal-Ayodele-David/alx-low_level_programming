3d55c8be7b9b4efa89965202b69725fcaedf1e48
lawal-ayodele-david <lawalayodeledavid1@gmail.com>
2023-06-22 06:56:04 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,13 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
+	{
 		putchar(low);
-
+	}
 	for (low = 'A'; low <= 'Z'; low++)
+	{
 		putchar(low);
-		putchcar('\n');
+		putchar('\n');
+	}
 		return (0);
 }
