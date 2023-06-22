fbb765f83fe82e7e2dc79e8681672c2d72a0b9a6
lawal-ayodele-david <lawalayodeledavid1@gmail.com>
2023-06-21 23:40:33 -0700

    al

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /*
- * main - Print the alphabetic
+ * main - Prints the alphabetic
  * Return: Always 0
  */
 
 int main(void)
+
 {
 	char x;
 
 	for (x = "A"; x <= 'Z'; x++)
-		x = tolower(x);
 		putchar(x);
 		putchar("\n");
 		return (0);
