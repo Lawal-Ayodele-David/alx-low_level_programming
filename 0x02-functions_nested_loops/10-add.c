17931c4ff738b3136f4b24373d9d23734bfccf56
lawal-ayodele-david <lawalayodeledavid1@gmail.com>
2023-06-27 08:59:34 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - This function takes in two numbers as arguments and add them.
+ * @num1: This is the first argument and the first integer to be added.
+ * @num2: This is the second argument and the second integer to be added.
+ * Return: The result of the addition
+*/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
